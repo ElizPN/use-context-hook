577b3ea0b5f48d2b6edb5f0b3f964adcff46a59b
ElizPN <pankevych.liza@gmail.com>
2023-01-30 14:21:07 +0100

    change state by clicking on button

--- src/Component1.tsx
+++ src/Component1.tsx
@@ -7,9 +7,16 @@ export const UserContext = createContext("");
 export function Component1() {
   const [user, setUser] = useState("Jesse Hall");
 
+  const handlerClick = () => {
+    setUser("Liza Romanova");
+  };
+
   return (
     <UserContext.Provider value={user}>
       <h1>{`Hello ${user}!`}</h1>
+      <button type='button' onClick={handlerClick}>
+        Click Me!
+      </button>
       <Component2 />
     </UserContext.Provider>
   );
