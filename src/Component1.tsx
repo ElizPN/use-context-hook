1f0151ef5c28379b79d61c868781ee78252e7e1e
ElizPN <pankevych.liza@gmail.com>
2023-01-30 14:06:25 +0100

    refactor , move child components to separate files

--- src/Component1.tsx
+++ src/Component1.tsx
@@ -1,7 +1,8 @@
-import { useState, createContext, useContext } from "react";
+import { useState, createContext } from "react";
 import ReactDOM from "react-dom/client";
+import { Component2 } from "./Component2";
 
-const UserContext = createContext("");
+export const UserContext = createContext("");
 
 export function Component1() {
   const [user, setUser] = useState("Jesse Hall");
@@ -13,44 +14,3 @@ export function Component1() {
     </UserContext.Provider>
   );
 }
-
-function Component2() {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3 />
-    </>
-  );
-}
-
-function Component3() {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4 />
-    </>
-  );
-}
-
-function Component4() {
-  return (
-    <>
-      <h1>Component 4</h1>
-      <Component5 />
-    </>
-  );
-}
-
-function Component5() {
-  const user = useContext(UserContext);
-
-  return (
-    <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user} again!`}</h2>
-    </>
-  );
-}
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<Component1 />);
---
+++ src/Component4.tsx
@@ -0,0 +1,10 @@
+import { Component5 } from "./Component5";
+
+export function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
